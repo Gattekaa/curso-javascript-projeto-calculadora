ad70e12efaa6c8f527c22df73d26e097192a8a59
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-09-14 17:15:39 -0300

    Create initialize() in CalcController

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,28 @@
 class CalcController {
+
+    constructor() {
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize() {
+
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
     
+    set displayCalc(valor){
+        this.displayCalc = valor;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(valor) {
+        this.currentDate = valor;
+    }
 }
