09d72934aba105a5ad14619767137abd125f7119
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-09-16 13:18:47 -0300

    Added error handling method.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -193,7 +193,13 @@ class CalcController {
 
     getResult() {
 
-        return eval(this._operation.join(""));
+        try{
+            return eval(this._operation.join(""));
+        } catch(e) {
+            return;
+        }
+
+        
     }
 
     calc() {
